0408cf9c12866a4d8b3f7b08b41446798f4259ef
Robert Taylor <robertntaylor123@gmail.com>
2023-10-07 13:50:38 -0600

    Add react globe

--- green-alberta/src/index.js
+++ green-alberta/src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Globe from 'globe.gl';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const gData = [...Array(1000).keys()].map(() => ({
+  lat: (Math.random() - 0.5) * 180,
+  lng: (Math.random() - 0.5) * 360,
+  size: Math.random() / 3,
+  color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+}));
+
+const myGlobe = Globe();
+myGlobe(document.getElementById('globeViz'))
+  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+  .pointsData(gData);
+
 root.render(
   <React.StrictMode>
     <App />
+    <Globe
+      pointsData={gData}
+    />, 
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
